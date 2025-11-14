60e176e0fc17e9957d10e5c595a2c88a7f0a012b
Prathamesh Dabhade <prathameshdabhade71@gmail.com>
2025-11-14 16:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const board = document.getElementById("board");
+const SQUARES = 800;
+
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseover", () => setColor(square));
+    square.addEventListener("mouseout", () => removeColor(square));
+
+    board.appendChild(square);
+}
+
+function setColor(square) {
+    const color = getRandomColor();
+    square.style.background = color;
+}
+
+function removeColor(square) {
+    // Fade-out happens via CSS transition
+    setTimeout(() => {
+        square.style.background = "#1d1d1d";
+    }, 1000);
+}
+
+function getRandomColor() {
+    return `hsl(${Math.random() * 360}, 70%, 50%)`;
+}
